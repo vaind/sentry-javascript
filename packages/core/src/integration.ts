02922e642f18cf710e436a7da67459fa1de45e5d
Katie Byers <lobsterkatie@gmail.com>
2022-09-01 16:45:20 -0700

    test first vs second wins

--- packages/core/src/integration.ts
+++ packages/core/src/integration.ts
@@ -1,6 +1,6 @@
 import { addGlobalEventProcessor, getCurrentHub } from '@sentry/hub';
 import { Integration, Options } from '@sentry/types';
-import { logger } from '@sentry/utils';
+import { arrayify, logger } from '@sentry/utils';
 
 export const installedIntegrations: string[] = [];
 
@@ -10,53 +10,190 @@ export type IntegrationIndex = {
 };
 
 /**
+ * Find the names of any integrations which are duplicated in the given array.
+ *
+ * Note: Each name only appears once in the result, no matter how many times an integration with that name appears in
+ * the array.
+ *
+ * @private
+ */
+// function findDuplicateIntegrationNames(integrations: Integration[]): string[] {
+//   const integrationNames = integrations.map(integration => integration.name);
+//
+//   // If a name is duplicated, then all instances but the last will have a match later on in the array.
+//   const duplicateNames = integrationNames.filter((name, index) => integrationNames.includes(name, index + 1));
+//
+//   // Use a set to dedupe the names, in case somehow an integration appeared more than twice in the original array
+//   return [...new Set(duplicateNames)];
+// }
+
+/**
+ * Remove duplicates from the given array, preferring the last instance of any duplicate. Not guaranteed to
+ * preseve the order of unmatched integrations.
+ *
  * @private
  */
 function filterDuplicates(integrations: Integration[]): Integration[] {
-  return integrations.reduce((acc, integrations) => {
-    if (acc.every(accIntegration => integrations.name !== accIntegration.name)) {
-      acc.push(integrations);
+  // function filterDuplicates(integrations: Integration[]): {
+  //   integrationNames: string[];
+  //   duplicateNames: string[];
+  //   dedupedIntegrations: Integration[];
+  // } {
+  // const duplicateNames = findDuplicateIntegrationNames(integrations);
+
+  // debugger;
+
+  // integrations.reverse();
+
+  const integrationsByName: { [key: string]: Integration } = {};
+  // const duplicateNames: Set<string> = new Set();
+
+  integrations.forEach(currentInstance => {
+    const { name } = currentInstance;
+
+    // if (name in integrationsByName) {
+    //   duplicateNames.add(name);
+    // }
+
+    const existingInstance = integrationsByName[name];
+
+    // We want integrations later in the array to overwrite earlier ones of the same type, except that we never want a
+    // default instance to overwrite an existing user instance
+    if (currentInstance.isDefaultInstance && existingInstance && !existingInstance.isDefaultInstance) {
+      return;
     }
-    return acc;
-  }, [] as Integration[]);
+
+    integrationsByName[name] = currentInstance;
+  });
+
+  return Object.values(integrationsByName);
+  // return Object.values(integrationsByName).reverse();
+
+  // return {
+  //   integrationNames: Object.keys(integrationsByName),
+  //   duplicateNames: [...duplicateNames],
+  //   dedupedIntegrations: Object.values(integrationsByName),
+  // };
 }
 
-/** Gets integration to install */
-export function getIntegrationsToSetup(options: Options): Integration[] {
-  const defaultIntegrations = (options.defaultIntegrations && [...options.defaultIntegrations]) || [];
-  const userIntegrations = options.integrations;
+declare module '@sentry/types' {
+  interface Integration {
+    isDefaultInstance?: boolean;
+  }
+}
 
-  let integrations: Integration[] = [...filterDuplicates(defaultIntegrations)];
+/** Gets integrations to install */
+export function getIntegrationsToSetup(options: Options): Integration[] {
+  // const { defaultIntegrations = [], integrations: userIntegrations } = options;
+
+  const defaultIntegrations = options.defaultIntegrations || [];
+  const userIntegrations = options.integrations;
+  // debugger;
+
+  // We flag default instances, so that later we can tell them apart from any user-created instances of the same class
+  defaultIntegrations.forEach(integration => {
+    integration.isDefaultInstance = true;
+  });
+
+  let integrations: Integration[];
 
   if (Array.isArray(userIntegrations)) {
-    // Filter out integrations that are also included in user options
-    integrations = [
-      ...integrations.filter(integrations =>
-        userIntegrations.every(userIntegration => userIntegration.name !== integrations.name),
-      ),
-      // And filter out duplicated user options integrations
-      ...filterDuplicates(userIntegrations),
-    ];
+    integrations = [...defaultIntegrations, ...userIntegrations];
   } else if (typeof userIntegrations === 'function') {
-    integrations = userIntegrations(integrations);
-    integrations = Array.isArray(integrations) ? integrations : [integrations];
+    integrations = arrayify(userIntegrations(defaultIntegrations));
+  } else {
+    integrations = defaultIntegrations;
   }
+
+  // At this point, `integrationsToInstall` may include two copies of the same integration, one default and one provided
+  // by the user. If the user provided a function, we don't know what order any duplicates are in, so we can't make a
+  // simple rule like "most recent wins" the way we could with an object. We therefore have to manually implement an "if
+  // it's a duplicate, take the user's version" rule.
+
+  const finalIntegrations = filterDuplicates(integrations);
+  // const { integrationNames, duplicateNames, dedupedIntegrations: finalIntegrations } = filterDuplicates(integrations);
+
+  const debugIndex = finalIntegrations.findIndex(integration => integration.name === 'Debug');
+  if (debugIndex !== -1) {
+    const debugInstance = finalIntegrations.splice(debugIndex, 1)[0];
+    finalIntegrations.push(debugInstance);
+  }
+
+  //   // const integrationNames = integrations.map(i => i.name);
+  //   // const duplicateNames = integrationNames.filter((name, index) =>
+  //   //   // Duplicates will have a match later on in the array (the second argument to `includes` tells it where to start
+  //   //   // looking)
+  //   //   integrationNames.includes(name, index + 1),
+  //   // );
+  //   const isPreferredInstance = (integration: Integration): boolean =>
+  //     !duplicateNames.includes(integration.name) || integration.isDefaultInstance !== true;
+  //
+  //   // The `Debug` integration always has to be last, so we make the array without it and then add it to the end if needed
+  //   const finalIntegrations = integrations.filter(
+  //     integration => integration.name !== 'Debug' && isPreferredInstance(integration),
+  //   );
+  //   if (integrationNames.includes('Debug')) {
+  //     const debugInstance = integrations.find(
+  //       integration => integration.name === 'Debug' && isPreferredInstance(integration),
+  //     ) as Integration;
+  //     finalIntegrations.push(debugInstance);
+  //   }
+
+  return finalIntegrations;
+
+  // Grab t
+
+  //   const finalIntegrations = integrations.filter(
+  //     integration =>
+  //       // The `Debug` integration always has to be last, so we make the array without it and then add it at the end
+  //       integration.name !== 'Debug' &&
+  //       // For all others, we want them if they're either non-duplicates or the non-default copy of one that is duplicated
+  //       (!duplicateNames.includes(integration.name) || integration.default !== true),
+  //   );
+  //
+  //   if (integrationNames.includes('Debug')) {
+  //     const debugInstance = integrations.find(
+  //       integration =>
+  //         integration.name === 'Debug' && (!duplicateNames.includes(integration.name) || integration.default !== true),
+  //     );
+  //     finalIntegrations.concat(debugInstance);
+  //   }
+
+  // if (integrationNames.length > new Set(integrationNames).size) {
+  //   userIntegrations = userIntegrations.fi;
+  // }
+
+  // const defaultIntegrations = (options.defaultIntegrations && [...options.defaultIntegrations]) || [];
+
+  // let integrationsToInstall: Integration[] = [...filterDuplicates(defaultIntegrations)];
+  //
+  //   if (Array.isArray(userIntegrations)) {
+  //     // Filter out integrations that are also included in user options
+  //     integrationsToInstall = [
+  //       ...defaultIntegrations.filter(defaultIntegration =>
+  //         userIntegrations.every(userIntegration => userIntegration.name !== defaultIntegration.name),
+  //       ),
+  //       // And filter out duplicated user options integrations
+  //       ...filterDuplicates(userIntegrations),
+  //     ];
+  //   } else if (typeof userIntegrations === 'function') {
+  //     integrationsToInstall = userIntegrations(integrationsToInstall);
+  //     integrationsToInstall = Array.isArray(integrationsToInstall) ? integrationsToInstall : [integrationsToInstall];
+  //   }
 
   // Make sure that if present, `Debug` integration will always run last
-  const integrationsNames = integrations.map(i => i.name);
-  const alwaysLastToRun = 'Debug';
-  if (integrationsNames.indexOf(alwaysLastToRun) !== -1) {
-    integrations.push(...integrations.splice(integrationsNames.indexOf(alwaysLastToRun), 1));
-  }
-
-  return integrations;
+  //   const alwaysLastToRun = 'Debug';
+  //   if (integrationNames.indexOf(alwaysLastToRun) !== -1) {
+  //     integrations.push(...integrations.splice(integrationNames.indexOf(alwaysLastToRun), 1));
+  //   }
+  //
+  //   return integrations;
 }
 
 /**
- * Given a list of integration instances this installs them all. When `withDefaults` is set to `true` then all default
- * integrations are added unless they were already provided before.
+ * Installs the given integration instances.
+ *
  * @param integrations array of integration instances
- * @param withDefault should enable default integrations
  */
 export function setupIntegrations(integrations: Integration[]): IntegrationIndex {
   const integrationIndex: IntegrationIndex = {};
--- packages/core/test/lib/integration.test.ts
+++ packages/core/test/lib/integration.test.ts
@@ -1,4 +1,4 @@
-import { Integration } from '@sentry/types';
+import { Integration, Options } from '@sentry/types';
 
 import { getIntegrationsToSetup } from '../../src/integration';
 
@@ -6,8 +6,12 @@ import { getIntegrationsToSetup } from '../../src/integration';
 class MockIntegration implements Integration {
   public name: string;
 
-  public constructor(name: string) {
+  // Only for testing - tag to keep separate instances straight when testing deduplication
+  public tag?: number;
+
+  public constructor(name: string, tag?: number) {
     this.name = name;
+    this.tag = tag;
   }
 
   public setupOnce(): void {
@@ -15,8 +19,172 @@ class MockIntegration implements Integration {
   }
 }
 
+// function expectCorrectNames(options: Options, expected: string[]): void {
+//   const integrations = getIntegrationsToSetup(options);
+//   expect(integrations.map(i => i.name)).toEqual(expected);
+// }
+//
+// function expectCorrectNamesAndTags(options: Options, expected: string[]): void {
+//   const integrations = getIntegrationsToSetup(options) as MockIntegration[];
+//   expect(integrations.map(i => [i.name, i.tag])).toEqual(expected);
+// }
+
+// all 3: no dups in default, dups in default, empty default, missing default
+// first 2: no dups in user, dups in user, dups between default and user default first, internal dups and not
+// function: dups between default and user user first
+
 describe('getIntegrationsToSetup', () => {
-  it('works with empty array', () => {
+  describe('no duplicates', () => {
+    const defaultIntegrations = [new MockIntegration('ChaseSquirrels')];
+    const userIntegrationsArray = [new MockIntegration('CatchTreats')];
+    const userIntegrationsFunction = (defaults: Integration[]) => [...defaults, ...userIntegrationsArray];
+
+    test.each([
+      ['no default integrations, no user integrations provided', false, undefined, []],
+      ['no default integrations, empty user-provided array', false, [], []],
+      ['no default integrations, user-provided array', false, userIntegrationsArray, ['CatchTreats']],
+      ['no default integrations, user-provided function', false, userIntegrationsFunction, ['CatchTreats']],
+      ['with default integrations, no user integrations provided', defaultIntegrations, undefined, ['ChaseSquirrels']],
+      ['with default integrations, empty user-provided array', defaultIntegrations, [], ['ChaseSquirrels']],
+      [
+        'with default integrations, user-provided array',
+        defaultIntegrations,
+        userIntegrationsArray,
+        ['ChaseSquirrels', 'CatchTreats'],
+      ],
+      [
+        'with default integrations, user-provided function',
+        defaultIntegrations,
+        userIntegrationsFunction,
+        ['ChaseSquirrels', 'CatchTreats'],
+      ],
+    ])('%s', (_, defaultIntegrations, userIntegrations, expected) => {
+      const integrations = getIntegrationsToSetup({
+        // We have to cast in order not to have to do `false as const` in every test case
+        defaultIntegrations: defaultIntegrations as Options['defaultIntegrations'],
+        integrations: userIntegrations,
+      });
+      expect(integrations.map(i => i.name)).toEqual(expected);
+    });
+  });
+  describe('deduping', () => {
+    //   dups from default (a)
+    //   dups from user (a, f)
+    //   dups between but not within(a, f both orders)
+    //   dups between and in default (a, f both orders)
+    // dups between and in user(a, f both orders)
+    // dups between and in both (a, f both orders)
+
+    const defaultIntegrations = [new MockIntegration('ChaseSquirrels', 1)];
+    const duplicateDefaultIntegrations = [
+      new MockIntegration('ChaseSquirrels', 1),
+      new MockIntegration('ChaseSquirrels', 2),
+    ];
+
+    const userIntegrationsArray = [new MockIntegration('CatchTreats', 1)];
+    const duplicateUserIntegrationsArray = [
+      new MockIntegration('CatchTreats', 1),
+      new MockIntegration('CatchTreats', 2),
+    ];
+    const userIntegrationsMatchingDefaultsArray = [
+      new MockIntegration('ChaseSquirrels', 3),
+      new MockIntegration('CatchTreats', 1),
+    ];
+
+    // const userIntegrationsFunctionDefaultsFirst = (defaults: Integration[]) => [...defaults, ...userIntegrationsArray];
+    // const userIntegrationsFunctionDefaultsSecond = (defaults: Integration[]) => [...userIntegrationsArray, ...defaults];
+    const duplicateUserIntegrationsFunctionDefaultsFirst = (defaults: Integration[]) => [
+      ...defaults,
+      ...duplicateUserIntegrationsArray,
+    ];
+    const duplicateUserIntegrationsFunctionDefaultsSecond = (defaults: Integration[]) => [
+      ...duplicateUserIntegrationsArray,
+      ...defaults,
+    ];
+    const userIntegrationsMatchingDefaultsFunctionDefaultsFirst = (defaults: Integration[]) => [
+      ...defaults,
+      ...userIntegrationsMatchingDefaultsArray,
+    ];
+    const userIntegrationsMatchingDefaultsFunctionDefaultsSecond = (defaults: Integration[]) => [
+      ...userIntegrationsMatchingDefaultsArray,
+      ...defaults,
+    ];
+
+    test.each([
+      [
+        'duplicate default integrations',
+        duplicateDefaultIntegrations,
+        userIntegrationsArray,
+        [
+          ['ChaseSquirrels', 2],
+          ['CatchTreats', 1],
+        ],
+      ],
+      [
+        'duplicate user integrations, user-provided array',
+        defaultIntegrations,
+        duplicateUserIntegrationsArray,
+        [
+          ['ChaseSquirrels', 1],
+          ['CatchTreats', 2],
+        ],
+      ],
+      [
+        'duplicate user integrations, user-provided function with defaults first',
+        defaultIntegrations,
+        duplicateUserIntegrationsFunctionDefaultsFirst,
+        [
+          ['ChaseSquirrels', 1],
+          ['CatchTreats', 2],
+        ],
+      ],
+      [
+        'duplicate user integrations, user-provided function with defaults second',
+        defaultIntegrations,
+        duplicateUserIntegrationsFunctionDefaultsSecond,
+        [
+          ['CatchTreats', 2],
+          ['ChaseSquirrels', 1],
+        ],
+      ],
+      [
+        'same integration in default and user integrations, user-provided array',
+        defaultIntegrations,
+        userIntegrationsMatchingDefaultsArray,
+        [
+          ['ChaseSquirrels', 3],
+          ['CatchTreats', 1],
+        ],
+      ],
+      [
+        'same integration in default and user integrations, user-provided function with defaults first',
+        defaultIntegrations,
+        userIntegrationsMatchingDefaultsFunctionDefaultsFirst,
+        [
+          ['ChaseSquirrels', 3],
+          ['CatchTreats', 1],
+        ],
+      ],
+      [
+        'same integration in default and user integrations, user-provided function with defaults second',
+        defaultIntegrations,
+        userIntegrationsMatchingDefaultsFunctionDefaultsSecond,
+        [
+          ['ChaseSquirrels', 3],
+          ['CatchTreats', 1],
+        ],
+      ],
+    ])('%s', (_, defaultIntegrations, userIntegrations, expected) => {
+      const integrations = getIntegrationsToSetup({
+        defaultIntegrations: defaultIntegrations,
+        integrations: userIntegrations,
+      }) as MockIntegration[];
+      expect(integrations.map(i => [i.name, i.tag])).toEqual(expected);
+    });
+  });
+  describe('order of debug', () => {});
+
+  it('works when user provides an empty array', () => {
     const integrations = getIntegrationsToSetup({
       integrations: [],
     });
@@ -24,7 +192,7 @@ describe('getIntegrationsToSetup', () => {
     expect(integrations.map(i => i.name)).toEqual([]);
   });
 
-  it('works with single item', () => {
+  it('works when user provides an array with one item', () => {
     const integrations = getIntegrationsToSetup({
       integrations: [new MockIntegration('foo')],
     });
@@ -32,7 +200,7 @@ describe('getIntegrationsToSetup', () => {
     expect(integrations.map(i => i.name)).toEqual(['foo']);
   });
 
-  it('works with multiple items', () => {
+  it('works when user provides an array with multiple items', () => {
     const integrations = getIntegrationsToSetup({
       integrations: [new MockIntegration('foo'), new MockIntegration('bar')],
     });
@@ -40,7 +208,7 @@ describe('getIntegrationsToSetup', () => {
     expect(integrations.map(i => i.name)).toEqual(['foo', 'bar']);
   });
 
-  it('filter duplicated items', () => {
+  it('filters duplicated items given by user ', () => {
     const integrations = getIntegrationsToSetup({
       integrations: [new MockIntegration('foo'), new MockIntegration('foo'), new MockIntegration('bar')],
     });
@@ -48,7 +216,7 @@ describe('getIntegrationsToSetup', () => {
     expect(integrations.map(i => i.name)).toEqual(['foo', 'bar']);
   });
 
-  it('filter duplicated items and always let first win', () => {
+  it.skip('filter duplicated items and always let first win', () => {
     const first = new MockIntegration('foo');
     (first as any).order = 'first';
     const second = new MockIntegration('foo');
