2c0e0dcc6fbb60fbf3c0eec8eea8117e9322ba64
Katie Byers <lobsterkatie@gmail.com>
2022-09-26 14:11:41 -0700

    delete unneeded packages

--- packages/node-integration-tests/jest.setup.js
+++
@@ -1,2 +0,0 @@
-// Increases test timeout from 5s to 45s
-jest.setTimeout(45000);
--- packages/remix/jest.config.js
+++
@@ -1,6 +0,0 @@
-const baseConfig = require('../../jest/jest.config.js');
-
-module.exports = {
-  ...baseConfig,
-  testPathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/node_modules/', '<rootDir>/test/integration/'],
-};
