e4ce21f7d40fb09950324109d0f1ac23da723fab
Abhijeet Prasad <aprasad@sentry.io>
2022-10-05 12:21:29 +0200

    profiling test

--- jest/jest.config.js
+++ jest/jest.config.js
@@ -1,6 +1,25 @@
 module.exports = {
   // this is the package root, even when tests are being run at the repo level
   rootDir: process.cwd(),
+  testEnvironment: './sentry.env.js',
+  testEnvironmentOptions: {
+    sentryConfig: {
+      // `init` will be passed to `Sentry.init()`
+      init: {
+        dsn: 'https://4957b5a52f3240069a795d67cc283ded@o447951.ingest.sentry.io/4503919927754752',
+        environment: process.env.CI ? 'ci' : 'local',
+        tracesSampleRate: 1.0,
+      },
+
+      transactionOptions: {
+        // `tags` will be used for the test suite transaction
+        tags: {
+          branch: process.env.GITHUB_REF,
+          commit: process.env.GITHUB_SHA,
+        },
+      },
+    },
+  },
   collectCoverage: true,
   transform: {
     '^.+\\.ts$': 'ts-jest',
