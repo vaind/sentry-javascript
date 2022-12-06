735cfd1a13829d8d787cfdd91a07e79b62dad48a
Katie Byers <lobsterkatie@gmail.com>
2022-12-05 19:43:45 -0800

    fix tests

--- packages/replay/test/fixtures/error.ts
+++ packages/replay/test/fixtures/error.ts
@@ -27,6 +27,7 @@ export function Error(obj?: Event): any {
             data: {
               function: 'setTimeout',
             },
+            other: { caughtByUser: false },
           },
         },
       ],
